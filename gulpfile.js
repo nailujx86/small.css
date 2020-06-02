80b8079f6dd06bee9e52b11c640d5919cbb9e34c
Julian B <julian.blazek@outlook.de>
2020-06-02 20:52:03 +0200

    Update gulpfile and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,31 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var zopfli= require('gulp-zopfli-green');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var fs = require('fs-extra');
 
-gulp.task('default', function() {
+gulp.task('clean', (done) => {
+    fs.removeSync('dist');
+    fs.removeSync('build');
+    done();
+});
+
+gulp.task('minify_js', () => {
+    return gulp.src('src/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('build/optimized'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/minified'))
+        .pipe(gulp.dest('dist'))
+        .pipe(zopfli())
+        .pipe(gulp.dest('dist'));
+    done();
+})
+
+gulp.task('minify_css', () => {
     return gulp.src('src/*.css')
         .pipe(cleanCSS({level: 2}))
         .pipe(gulp.dest('dist/'))
@@ -10,3 +33,5 @@ gulp.task('default', function() {
         .pipe(gulp.dest('dist/'));
     done();
 });
+
+gulp.task('default', gulp.series('clean', gulp.parallel('minify_css', 'minify_js')));
